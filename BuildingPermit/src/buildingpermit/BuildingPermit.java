c5100b3c79861560a1a8731ec36cbf23373c8448
aqsak <aqsak@10.16.1.191>
2023-06-21 11:07:40 -0400

    writing a code for BuildingPermit

---
+++ BuildingPermit/src/buildingpermit/BuildingPermit.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege;
+
+/**
+ *
+ * @author orlan
+ */
+public class BuildingPermit {
+    public enum permits {BP2023-001, BP2023-002, BP2023-003, BP2023-004};
+    public enum categories = { Residential, Commercial, Industrial, Agricultural };
+    
+    private Permit permit;
+    private Category category;
+
+    public BuildingPermit(Permit permit, Category category) {
+        this.permit = Permit;
+        this.category = Category;
+    }
+    
+    public Permit getPermits() {
+        return permits;
+    }
+    
+    
+    public Categories getCategories() {
+        return categories;
+    }
+
+    public void setPermit(Permit permit) {
+        this.Permit = Permit;
+    }
+
+    public void setCategory(Category category) {
+        this.Category = Category;
+    }
+
+    public String getPermit() {
+        return Permit;
+    }
+
+    public Category getCategory() {
+        return Category;
+    }
+    
+}
+
+
+
